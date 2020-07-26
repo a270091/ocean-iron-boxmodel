024d45ccae8a3c03be1eb23ee87b1248967dc39b
Christoph Voelker <christoph.voelker@awi.de>
2020-07-26 11:46:03 +0200

    made PO4 plot for all 4 model runs nicer

--- plot_po4_allmodels.m
+++ plot_po4_allmodels.m
@@ -41,18 +41,86 @@ boxmodel_init_params
 po4_woa = params.po4init;
 
 figure(1)
+clf
 h0 = plot(po4_woa, po4_cl,'x');
-set(h0,'LineWidth',4,'MarkerSize',10,'Color',[0.6 0.2 0.1]);
+set(h0,'LineWidth',2,'MarkerSize',10,'Color',[0.6 0.2 0.1]);
 hold on 
 h1 = plot(po4_woa, po4_1l,'x');
-set(h1,'LineWidth',3,'MarkerSize',10,'Color',[0 0.4470 0.7410]);
+set(h1,'LineWidth',2,'MarkerSize',10,'Color',[0 0.4470 0.7410]);
 h2 = plot(po4_woa, po4_2l1,'x');
-set(h2,'LineWidth',3,'MarkerSize',10,'Color',[0.8290 0.5940 0.0250]);
+set(h2,'LineWidth',2,'MarkerSize',10,'Color',[0.8290 0.5940 0.0250]);
 h3 = plot(po4_woa, po4_2l2,'x');
-set(h3,'LineWidth',3,'MarkerSize',10,'Color',[0.3660 0.5740 0.0880]);
+set(h3,'LineWidth',2,'MarkerSize',10,'Color',[0.3660 0.5740 0.0880]);
 set(gca,'PlotBoxAspectRatio',[1 1 1],'FontSize',12);
 xlabel('WOA-derived PO_4');
 ylabel('model PO_4');
 yl = get(gca,'YLim');
 hd = plot(yl,yl,'k');
+
+% hand-edited annotations
+for k=1:12
+    xpos = [po4_woa(k),po4_woa(k)];
+    if (k==1)
+        yp1 = min([po4_cl(k),po4_1l(k),po4_2l1(k),po4_2l2(k)]);
+        yp2 = 0.5*yl(2);
+        ypos = ([yp1,yp2]);
+    elseif (k==2)
+        yp1 = min([po4_cl(k),po4_1l(k),po4_2l1(k),po4_2l2(k)]);
+        yp2 = 0.7*yl(2);
+        ypos = ([yp1,yp2]);
+    elseif (k==3)
+        yp1 = min([po4_cl(k),po4_1l(k),po4_2l1(k),po4_2l2(k)]);
+        yp2 = 0.6*yl(2);
+        ypos = ([yp1,yp2]);
+    elseif (k==4)
+        yp1 = min([po4_cl(k),po4_1l(k),po4_2l1(k),po4_2l2(k)]);
+        yp2 = 0.4*yl(2);
+        ypos = ([yp1,yp2]);
+    elseif (k==5)
+        yp1 = min([po4_cl(k),po4_1l(k),po4_2l1(k),po4_2l2(k)]);
+        yp2 = 0.2*yl(2);
+        ypos = ([yp1,yp2]);
+    elseif (k==6)
+        yp1 = max([po4_cl(k),po4_1l(k),po4_2l1(k),po4_2l2(k)]);
+        yp2 = 0.3*yl(2);
+        ypos = ([yp1,yp2]);
+    elseif (k==7)
+        yp1 = max([po4_cl(k),po4_1l(k),po4_2l1(k),po4_2l2(k)]);
+        yp2 = 0.2*yl(2);
+        ypos = ([yp1,yp2]);
+    elseif (k==8)
+        yp1 = max([po4_cl(k),po4_1l(k),po4_2l1(k),po4_2l2(k)]);
+        yp2 = 0.8*yl(2);
+        ypos = ([yp1,yp2]);
+    elseif (k==9)
+        yp1 = max([po4_cl(k),po4_1l(k),po4_2l1(k),po4_2l2(k)]);
+        yp2 = 0.4*yl(2);
+        ypos = ([yp1,yp2]);
+    elseif (k==10)
+        yp1 = min([po4_cl(k),po4_1l(k),po4_2l1(k),po4_2l2(k)]);
+        yp2 = 0.8*yl(2);
+        ypos = ([yp1,yp2]);
+    elseif (k==11)
+        yp1 = max([po4_cl(k),po4_1l(k),po4_2l1(k),po4_2l2(k)]);
+        yp2 = 0.6*yl(2);
+        ypos = ([yp1,yp2]);
+    elseif (k==12)
+        yp1 = min([po4_cl(k),po4_1l(k),po4_2l1(k),po4_2l2(k)]);
+        yp2 = 0.9*yl(2);
+        ypos = ([yp1,yp2]);
+    else
+        ypos = yl;
+    end
+    ha = plot(xpos,ypos,'k');
+    ht = text(xpos(2),ypos(2),params.names{k});
+    if (ypos(2)>xpos(2))
+        set(ht,'FontSize',12 ,'Margin',5,'horizontalAlignment','right',...
+            'verticalAlignment','bottom')
+    else
+        set(ht,'FontSize',12 ,'Margin',5,'horizontalAlignment','left',...
+            'verticalAlignment','top')
+    end
+end
+
+print('PO4_all4boxmodels.png','-dpng')
 
